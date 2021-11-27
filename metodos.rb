ac217a9998bb08fe1b083cf777aed1404c29bfd5
Danillo <danillo.targino@gmail.com>
2021-11-27 14:18:50 -0400

    adicionando metodos.md

---
+++ metodos.rb
@@ -0,0 +1,17 @@
+def numeros_impares(min = 0, max)
+    (min..max).each do |numero|
+     puts "O número #{numero} é ímpar" if numero.odd?
+    end
+end
+
+def media(elementos)
+    elementos.sum / elementos.size
+end
+
+
+numeros_impares(80, 90)
+puts '---'
+numeros_impares(12)
+
+# puts media([9, 1, 2])
+# puts media([10, 20])
